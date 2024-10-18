47bdeedc36245f032500c09f66aca796a48d75d7
MD Taifur <Taifurislamashraf@gmail.com>
2024-10-18 15:11:48 +0600

    [add] facebook pixel setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import ReduxProvider from "@/lib/ReduxProvider";
 import { allkeywords, descriptionShop } from "@/lib/contstens";
 import { cn } from "@/lib/utils";
-import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Toaster } from "react-hot-toast";
@@ -25,12 +25,33 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-M7RXHBCL" />
       <body className={cn(poppins.className, "w-full")}>
         <ReduxProvider>
           <div className="max-w-[1400px] mx-auto">
             {children}
             <Toaster position="top-center" reverseOrder={false} />
+            <Script id="facebook-pixel" strategy="afterInteractive">
+              {`
+          !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '905675795011269');
+fbq('track', 'PageView');
+        `}
+            </Script>
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                src="https://www.facebook.com/tr?id=905675795011269&ev=PageView&noscript=1"
+              />
+            </noscript>
           </div>
         </ReduxProvider>
       </body>
