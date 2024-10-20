7a3956de021a43393f7ed0b141c893dcb886aff7
MD Taifur <Taifurislamashraf@gmail.com>
2024-10-20 12:40:10 +0600

    [change] condition for see more button

--- src/components/MixProdcts.tsx
+++ src/components/MixProdcts.tsx
@@ -17,13 +17,11 @@ const MixProdcts = async () => {
         ))}
       </div>
 
-      {data?.products?.length > 10 && (
-        <Link href={"/products"} className="my-4 underline w-[160px] mx-auto">
-          <Button variant={"outline"} size={"sm"}>
-            See More
-          </Button>
-        </Link>
-      )}
+      <Link href={"/products"} className="my-4 underline w-[160px] mx-auto">
+        <Button variant={"outline"} size={"sm"}>
+          See More
+        </Button>
+      </Link>
     </div>
   );
 };
