43b4626c5103940cd9abe39e230d3346542ce8ff
MD Taifur <Taifurislamashraf@gmail.com>
2024-10-29 00:00:48 +0600

    [add] google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800"],
@@ -30,31 +31,18 @@ export default async function RootLayout({
           <div className="max-w-[1400px] mx-auto">
             {children}
             <Toaster position="top-center" reverseOrder={false} />
-            <Script id="facebook-pixel" strategy="afterInteractive">
-              {`
-          !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '905675795011269');
-fbq('track', 'PageView');
-        `}
-            </Script>
-            <noscript>
-              <img
-                height="1"
-                width="1"
-                style={{ display: "none" }}
-                src="https://www.facebook.com/tr?id=905675795011269&ev=PageView&noscript=1"
-              />
-            </noscript>
           </div>
         </ReduxProvider>
       </body>
+      <GoogleTagManager gtmId="GTM-5TFNXQCX" />
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-5TFNXQCX"
+          height="0"
+          width="0"
+          style={{ display: "none" }}
+        ></iframe>
+      </noscript>
     </html>
   );
 }
